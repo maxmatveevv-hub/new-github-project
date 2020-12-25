390117c6b021d4a3718b70f121bb21be8a2a430f
maxmatveevv <max.matveevv@gmail.com>
2020-12-26 03:50:22 +0500

    lesson 7. Learn JS

---
+++ js/main.js
@@ -0,0 +1,67 @@
+// <noscript>
+//     Включите JavaScript
+// </noscript>
+
+// document.write("JavaScript говорит привет")
+// console.log("JavaScript говорит привет");
+// console.error("JavaScript говорит привет");
+// console.warn("JavaScript говорит привет");
+// console.info("JavaScript говорит привет");
+
+// var num = 5;
+// const consta = 7;
+//
+// var res = num + consta;
+// console.log("Переменная: " + (num - consta));
+//
+// var num_3 = 5;
+//
+// num_3 += 7;
+//
+// num_3++;
+//
+// console.log("результат: " + num_3);
+
+
+
+// var numt = Number("12");
+// var numt2 = Number("2");
+//
+// console.log("Результат: " + (numt + numt2));
+//
+//
+// console.log("Math: " + Math.PI);
+// console.log("Math: " + Math.min(3,4,5,6,7,8,89));
+
+
+//
+// var num = 15;
+// var isHaseHouse = true;
+//
+// if (num == 15 && isHaseHouse) {
+//     console.log("Верно");
+// }
+
+
+var stroka = "word";
+
+switch (stroka) {
+    case "4":
+        console.log("равно 4");
+    break;
+
+    case "word":
+        console.log("равно word");
+    break;
+
+    case "7":
+        console.log("равно 7");
+    break;
+
+    case "slovo":
+        console.log("равно slovo");
+    break;
+
+    default:
+        console.log("Default");
+}
